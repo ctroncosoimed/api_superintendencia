d097224fe134821527fd7f4ae071bdcfe3075e7a
CeTo <ctroncoso@i-med.cl>
2019-05-09 17:22:17 -0400

    add kue listen

--- config/kue.js
+++ config/kue.js
@@ -2,5 +2,6 @@ var kue = require('kue'),
   queue = kue.createQueue({
     redis: 'redis://redis'
   });
+kue.app.listen(process.env.PORT_KUE);
 
 console.log("KUE CONNECT IN PORT: "+process.env.PORT_KUE);
